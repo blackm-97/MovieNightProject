a1f979901ff22c52cc987b5e4db9d90457c92015
blackm-97 <blackm97@msu.edu>
2025-01-22 23:43:55 -0500

    Added Film Stats and better format

--- script.py
+++ script.py
@@ -31,9 +31,6 @@ def createuserList():
         new_list.append(line.strip())
     return new_list
 
-def lookForMovie(mov):
-    return 0
-
 # assign required credentials
 # assign URL
 
@@ -42,35 +39,86 @@ if __name__ == '__main__':
 
     user_list = createuserList()
     print(user_list)
+    movie_name = 'placeholder'
 
-    print('Enter the movie title:')
-    movie_name = input()
-    movie_name = changeNameToHTML(movie_name)
+    while movie_name:
+        print('Please enter the movie name. If there are two movies with the same name, insert the data afterwards.')
+        print('Keep the textbox blank to stop the program')
+        movie_name = input()
+        movie_name = changeNameToHTML(movie_name)
 
-    # Tests that movie exists
-    movie_test_doc = getHTMLdocument("https://letterboxd.com/film/" + movie_name +"/")
-    soup = BeautifulSoup(movie_test_doc, 'html.parser')
-    review = soup.find(attrs={"name": "description"})
+        # Tests that movie exists
+        movie_test_doc = getHTMLdocument("https://letterboxd.com/film/" + movie_name + "/")
+        soup = BeautifulSoup(movie_test_doc, 'html.parser')
 
-    #If movie exists, continue
-    if review:
-        for user in user_list:
-            url_to_scrape = "https://letterboxd.com/" + user + "/film/" + movie_name + "/"
+        genre_list = []
+        flag = False
 
-            # create document
-            html_document = getHTMLdocument(url_to_scrape)
+        film_stats = ''
 
-            # create soap object
-            soup = BeautifulSoup(html_document, 'html.parser')
+        try:
+            name = soup.find(attrs={"property": "og:title"}).get('content')
+            review = soup.find(attrs={"name": "description"})
+            year = soup.find(attrs={"id": "film-page-wrapper"})
+            year = year.find(attrs={"class": "releaseyear"}).a.string
+            genres = soup.find(attrs={"id": "film-page-wrapper"})
+            genres = genres.find(attrs={"id": "tab-genres"})
+            genres = genres.find(attrs={"class": "text-sluglist capitalize"}).find_all('a')
+            for genre in genres:
+                genre_list.append(genre.string)
 
-            try:
-                review = soup.find(attrs={"name": "description"})
-                rating = soup.find(attrs={"name": "twitter:data2"})
-                rating = getRating(str(rating.get('content')))
-                print(review.get('content'))
-                print(rating)
-            except:
-                print('User did not enter any rating')
-            # print(soup.find('div', 'film-metadata'))
-    else:
-        print('No Movie Found')
+            runtime = soup.find(attrs={"class": "text-link text-footer"}).contents[0].strip()
+            runtime = runtime[0:7]
+            print(name)
+            print("Genres: " + str(genre_list))
+            print("Runtime: " + runtime)
+            film_stats += "Film Stats: " + name[:-7] + ', ' + year
+            if genre_list[0]:
+                film_stats += ", " + genre_list[0]
+            if genre_list[1]:
+                film_stats += ", " + genre_list[1]
+            # print(year)
+            film_stats += ", " + runtime
+            print()
+            flag = True
+        except:
+            print("Failed to find movie")
+        review = soup.find(attrs={"name": "description"})
+
+        total_score = 0.0
+        total_reviews = 0.0
+
+        # If movie exists, continue
+        if flag:
+            for user in user_list:
+                url_to_scrape = "https://letterboxd.com/" + user + "/film/" + movie_name + "/"
+
+                # create document
+                html_document = getHTMLdocument(url_to_scrape)
+
+                # create soap object
+                soup = BeautifulSoup(html_document, 'html.parser')
+
+                try:
+                    review = soup.find(attrs={"name": "description"})
+                    rating = soup.find(attrs={"name": "twitter:data2"})
+                    rating = getRating(str(rating.get('content')))
+                    # print(review.get('content'))
+                    print('User: ' + user)
+                    print('Rating: ' + str(rating))
+                    print()
+                    total_reviews += 1
+                    total_score += float(rating)
+                except:
+                    print('User: ' + user)
+                    print('User did not enter any rating')
+                    print()
+                # print(soup.find('div', 'film-metadata'))
+            if total_reviews != 0:
+                print("Total Score:")
+                print(total_score / total_reviews)
+                print()
+                film_stats += ", " + str(total_score / total_reviews)
+                print(film_stats)
+            else:
+                print('No reviews found')
